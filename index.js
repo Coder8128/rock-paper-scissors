5fe4d3ee82f3f76e4e59735b074047e608a529d4
rahat <57500825+Coder8128@users.noreply.github.com>
2025-12-14 15:06:03 +0100

    Create initial working UI for the game

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+// Button creation and assigning event listeners
+let btnRock = document.querySelector(".rock");
+let btnPaper = document.querySelector(".paper");
+let btnScissors = document.querySelector(".scissors");
+
+btnRock.addEventListener("click", playRound);
+btnPaper.addEventListener("click", playRound);
+btnScissors.addEventListener("click", playRound);
+
+function check(e) {
+    console.log(e.target.value);
+}
+
+//Create tag elements
+const sect = document.querySelector(".resultArea");
+const compTxt = document.createElement("h2");
+const humnTxt = document.createElement("h2");
+const verdict = document.createElement("h2");
+
+verdict.textContent = "See if you can beat the computer";
+sect.appendChild(verdict);
+
+
+//Generate random computer choice
 function getComputerChoice() {
     let x = "rock";
     switch (Math.floor(Math.random() * 3)) {
@@ -18,68 +42,94 @@ function getHumanChoice() {
     return prompt("Input ur choice: ");
 }
 
+let humanScore = 0;
+let computerScore = 0;
+let gameRounds = 0;
 
-function playGame() {
+function playRound(e) {
 
-    let humanScore = 0;
-    let computerScore = 0;
+    let humn = e.target.value.toUpperCase();
+    let comp = getComputerChoice().toUpperCase();
+    // let comp = "ROCK";
 
-    function playRound(humanChoice, computerChoice) {
+    if ((comp == humn)) {
+        humanScore++;
 
-        let humn = humanChoice.toUpperCase();
-        let comp = computerChoice.toUpperCase();
+        compTxt.textContent = "Computer: " + comp;
+        humnTxt.textContent = "You draw: " + humn;
+        verdict.textContent = `It's a draw!`;
 
-        if (comp == "ROCK" && humn == "PAPER") {
-            console.log("The computer draws " + comp);
-            console.log("And you draw " + humn);
-            console.log("You win! Paper beats rock!")
-            humanScore++;
-        }
-        else if (comp == "PAPER" && humn == "SCISSORS") {
-            console.log("The computer draws " + comp);
-            console.log("And you draw " + humn);
-            console.log("You win! Scissors beat paper!")
-            humanScore++;
-        }
-        else if (comp == "SCISSORS" && humn == "ROCK") {
-            console.log("The computer draws " + comp);
-            console.log("And you draw " + humn);
-            console.log("You win! Rock beats scissors!")
-            humanScore++;
-        } //now losing
-        else if (comp == "SCISSORS" && humn == "PAPER") {
-            console.log("The computer draws " + comp);
-            console.log("And you draw " + humn);
-            console.log("You lose! Scissors beats paper!")
-            computerScore++;
-        }
-        else if (comp == "ROCK" && humn == "SCISSORS") {
-            console.log("The computer draws " + comp);
-            console.log("And you draw " + humn);
-            console.log("You lose! Rock beats scissors!")
-            computerScore++;
-        }
-        else if (comp == "PAPER" && humn == "ROCK") {
-            console.log("The computer draws " + comp);
-            console.log("And you draw " + humn);
-            console.log("You lose! Paper beats rock!")
-            computerScore++;
+        sect.appendChild(compTxt);
+        sect.appendChild(humnTxt);
+
+        sect.appendChild(verdict);
+    }
+    else if (comp == "ROCK" && humn == "PAPER") {
+        humanScore++;
+
+        compTxt.textContent = "Computer: " + comp;
+        humnTxt.textContent = "You draw: " + humn;
+        verdict.textContent = `You win ${humn} beats ${comp}`;
+
+        sect.appendChild(compTxt);
+        sect.appendChild(humnTxt);
+
+        sect.appendChild(verdict);
+
+    }
+    else if (comp == "PAPER" && humn == "SCISSORS") {
+        humanScore++;
+
+        compTxt.textContent = "Computer: " + comp;
+        humnTxt.textContent = "You draw: " + humn;
+        verdict.textContent = `You win ${humn} beats ${comp}`;
+
+        sect.appendChild(compTxt);
+        sect.appendChild(humnTxt);
+
+        sect.appendChild(verdict);
+    }
+    else if (comp == "SCISSORS" && humn == "ROCK") {
+        humanScore++;
+
+        compTxt.textContent = "Computer: " + comp;
+        humnTxt.textContent = "You draw: " + humn;
+        verdict.textContent = `You win ${humn} beats ${comp}`;
+
+        sect.appendChild(compTxt);
+        sect.appendChild(humnTxt);
+
+        sect.appendChild(verdict);
+    }
+    else {
+        computerScore++;
+
+        compTxt.textContent = "Computer: " + comp;
+        humnTxt.textContent = "You draw: " + humn;
+        verdict.textContent = `You lose! ${comp} beats ${humn}`;
+
+        sect.appendChild(compTxt);
+        sect.appendChild(humnTxt);
+
+        sect.appendChild(verdict);
+    }
+    gameRounds++;
+
+    //Final score comparator to determine the winner
+    if (gameRounds >= 5) {
+
+        sect.removeChild(compTxt);
+        sect.removeChild(humnTxt);
+
+        if (humanScore == computerScore) {
+            verdict.textContent = `Game over! Computer: ${computerScore} You: ${humanScore}`;
+        } else if (humanScore > computerScore) {
+            verdict.textContent = `You win! Computer: ${computerScore} You: ${humanScore}`;
         } else {
-            console.log("The computer draws " + comp);
-            console.log("And you draw " + humn);
-            console.log("It's a draw!")
+            verdict.textContent = `Game over! You lose! Computer: ${computerScore} You: ${humanScore}`;
         }
     }
-
-
-
-    for (let i = 0; i < 5; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-
-        playRound(humanSelection, computerSelection);
-    }
-
-    humanScore == computerScore ? console.log("It's a draw all the way! IMPOSSIBLE") : humanScore > computerScore ? console.log("You won the game!") : console.log("WOMP WOMP");
-
 }
+
+
+
